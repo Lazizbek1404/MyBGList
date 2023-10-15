36fbeeb7c58bceba94a9b9354bc09384192187dc
Lazizbek1404 <lazizbekanorqul14@gmail.com>
2023-10-15 09:25:53 +0500

    2.5-Exercise

---
+++ MyBGList_Solution/MyBGList/BoardGame.cs
@@ -0,0 +1,14 @@
+﻿using MyBGList.Controllers;
+
+namespace MyBGList
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Year { get; set; }
+        public int? MinPlayers { get; set; }
+        public int? MaxPlayers { get; set; }
+
+    }
+}
--- MyBGList_Solution/MyBGList/Program.cs
+++ MyBGList_Solution/MyBGList/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSwaggerGen(opts =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
